00dffc854aade04f4693c5e8e34b29828dbed457
MigueXavier <miguelxavier900@gmail.com>
2024-11-02 22:03:43 -0300

    Add files via upload

---
+++ portifólio/porte.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const imgs = document.getElementById("img");
+    const img = document.querySelectorAll("#img img");
+    let idx = 0;
+
+    function carrossel() {
+        idx++;
+        if (idx > img.length - 1) {
+            idx = 0;
+        }
+        imgs.style.transform = `translateX(${-idx * 100}%)`; // Mover as imagens em porcentagem
+    }
+
+    setInterval(carrossel, 1800);
+});
